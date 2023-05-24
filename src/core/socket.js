0a13de9b7c34c8a4a662db398219335c138e0ce9
Anton Ezerskiy <anton030701@gmail.com>
2023-05-24 20:22:21 +0300

    commit release

--- src/core/socket.js
+++ src/core/socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
 
-const socket = io(window.location.origin.replace("https://chat-steel-eight.vercel.app", "5000"));
+const socket = io(window.location.origin.replace("3000", "5000"));
 
 export default socket;
